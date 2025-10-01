8035bd42d9e4c1c41086e7956a29d81cf7d3b486
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-01 23:25:43 +0530

    bug: querying has stopped in prod
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,26 +1,25 @@
 "use client";
 
 import { useOrganization } from "@clerk/nextjs";
-import { use } from "react";
+import { useSearchParams } from "next/navigation";
 import EmptyOrg from "./_components/empty-org";
 import DrawList from "./_components/draw-list";
 
-interface DashboardPageProps {
-  searchParams: Promise<{
-    search?: string;
-    favorites?: string;
-  }>;
-}
-
-export default function DashboardPage({ searchParams }: DashboardPageProps) {
+export default function DashboardPage() {
   const { organization } = useOrganization();
-  const params = use(searchParams);
+  const searchParams = useSearchParams();
+  
+  const query = {
+    search: searchParams.get("search") || undefined,
+    favorites: searchParams.get("favorites") || undefined,
+  };
+  
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <DrawList orgId={organization.id} query={params} />
+        <DrawList orgId={organization.id} query={query} />
       )}
     </div>
   );
