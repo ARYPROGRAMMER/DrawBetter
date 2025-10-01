bc004aee77eb6340369d1e3055ae6ab85817a4f0
Arya Pratap Singh <notaryasingh@gmail.com>
2025-10-01 23:28:08 +0530

    bug: querying has stopped in prod (#31)
    * bug: querying has stopped in prod
    
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>
    
    * bug: querying has stopped in prod
    
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>
    
    * bug: querying has stopped in prod
    
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>
    
    ---------
    
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -11,7 +11,7 @@ export const SearchInput = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(searchParams.get("search") || "");
   const [debouncedValue] = useDebounceValue(value, 500);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -19,12 +19,14 @@ export const SearchInput = () => {
   };
 
   useEffect(() => {
+    const currentFavorites = searchParams.get("favorites");
+    
     const url = qs.stringifyUrl(
       {
         url: "/",
         query: {
           search: debouncedValue,
-          favorites: searchParams.get("favorites"),
+          favorites: currentFavorites,
         },
       },
       { skipEmptyString: true, skipNull: true }
